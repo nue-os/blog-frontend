63e971aac35857c0df969180b379decf5363c432
nue-os <kimsoeun0108@gmail.com>
2025-05-22 16:20:52 +0900

    feat: 메인 포스트 리스트 페이지 레이아웃 및 스타일링

---
+++ src/pages/PostListPage.jsx
@@ -0,0 +1,80 @@
+import { useCallback, useEffect, useRef, useState } from 'react'
+import PostCard from '../components/PostCard'
+
+const PostListPage = () => {
+  const [postList, setPostList] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  const [page, setPage] = useState(0)
+  const [hasMore, setHasMore] = useState(true)
+  const listRef = useRef(null)
+  const observer = useRef()
+
+  // 마지막 게시물 요소를 감지하는 ref 콜백
+  const lastPostElementRef = useCallback(
+    node => {
+      if (isLoading || !node) return
+      if (observer.current) observer.current.disconnect()
+
+      observer.current = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPage(prev => prev + 1)
+        }
+      })
+
+      observer.current.observe(node)
+    },
+    [isLoading, hasMore]
+  )
+
+  useEffect(() => {
+    const fetchPostList = () => {
+      try {
+        if (page > 0) setIsLoading(true)
+        // 임시
+        const data = {
+          posts: [
+            {
+              id: 1,
+              title: '제목1',
+              summary: '내용1',
+              author: '작성자1',
+              createdAt: '2025-05-22T00:00:00Z',
+            },
+          ],
+        }
+        setPostList(prev => (page === 0 ? data.posts : [...prev, ...data.posts]))
+        setHasMore(false)
+      } catch (err) {
+        console.error(err)
+        setError('글 목록을 불러오는데 실패했습니다.')
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchPostList()
+  })
+
+  return (
+    <main className="main">
+      <h2 className="heading2">글 목록</h2>
+      {error && <p>{error}</p>}
+      {isLoading && page === 0 ? (
+        <p>로딩 중 ...</p>
+      ) : postList.length === 0 ? (
+        <p>첫번 째 글의 주인공이 되어주세요.</p>
+      ) : (
+        <ul ref={listRef} className="flex flex-col gap-8">
+          {postList.map((post, index) => (
+            <li key={post.id} ref={index === postList.length - 1 ? lastPostElementRef : null}>
+              <PostCard post={post} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </main>
+  )
+}
+
+export default PostListPage
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import DefaultLayout from '../layout/DefaultLayout'
 import SignUpPage from '../pages/SignUpPage'
 import LoginPage from '../pages/LoginPage'
+import PostListPage from '../pages/PostListPage'
 
 export const router = createBrowserRouter([
   {
@@ -9,6 +10,10 @@ export const router = createBrowserRouter([
     element: <DefaultLayout />,
     errorElement: <div>에러</div>,
     children: [
+      {
+        index: true,
+        element: <PostListPage />,
+      },
       {
         path: '/signup',
         element: <SignUpPage />,
