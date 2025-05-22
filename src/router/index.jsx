d91d8b98610283407cd54403ac4f9a17994b5e2c
nue-os <kimsoeun0108@gmail.com>
2025-05-22 10:50:51 +0900

    chore: 라우터 설정

---
+++ src/router/index.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from 'react-router-dom'
+import DefaultLayout from '../layout/DefaultLayout'
+import SignUpPage from '../pages/SignUpPage'
+
+export const router = createBrowserRouter({
+  path: '/',
+  element: <DefaultLayout />,
+  errorElement: <div>에러</div>,
+  children: [
+    {
+      path: '/signup',
+      element: <SignUpPage />,
+    },
+  ],
+})
