62dd530454af95fb543b070c25d5b42df125a367
nue-os <kimsoeun0108@gmail.com>
2025-05-26 09:10:51 +0900

    feat: zustand 세팅 및 사용자 정보 전역 상태 관리

---
+++ src/store/useUserStore.js
@@ -0,0 +1,23 @@
+import { create } from 'zustand'
+
+const getInitialUserId = () => {
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem('userId') || null
+  }
+  return null
+}
+const useUserStore = create(set => ({
+  userId: getInitialUserId(),
+  id: '',
+  setUserId: userId => {
+    localStorage.setItem('userId', userId)
+    set({ userId })
+  },
+  setId: id => set({ id }),
+  resetUser: () => {
+    localStorage.removeItem('userId')
+    set({ userId: null, id: '' })
+  },
+}))
+
+export default useUserStore
