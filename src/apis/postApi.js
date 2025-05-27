36aeffba14d524f11be89856e1994beb4ecbde98
nue-os <kimsoeun0108@gmail.com>
2025-05-27 10:30:54 +0900

    fix: API 엔드포인트 수정

--- src/apis/postApi.js
+++ src/apis/postApi.js
@@ -1,9 +1,9 @@
 import { deleteRequest, getRequest, postRequest, putRequest } from './api'
 
-export const createPost = postData => postRequest('/postWrite', postData)
-export const getPostList = (page = 0, limit = 3) => getRequest('/postList', { page, limit })
-export const getPostDetail = postId => getRequest(`/post/${postId}`)
-export const updatePost = (postId, postData) => putRequest(`/post/${postId}`, postData)
-export const deletePost = postId => deleteRequest(`/post/${postId}`)
+export const createPost = postData => postRequest('/posts', postData)
+export const getPostList = (page = 0, limit = 3) => getRequest('/posts', { page, limit })
+export const getPostDetail = postId => getRequest(`/posts/${postId}`)
+export const updatePost = (postId, postData) => putRequest(`/posts/${postId}`, postData)
+export const deletePost = postId => deleteRequest(`/posts/${postId}`)
 
-export const toggleLike = postId => postRequest(`/post/${postId}/like`)
+export const toggleLike = postId => postRequest(`/posts/${postId}/like`)
--- src/apis/userApi.js
+++ src/apis/userApi.js
@@ -1,12 +1,12 @@
 import { getRequest, postRequest, putRequest } from './api'
 
-export const signUp = userData => postRequest('/signUp', userData)
-export const login = userData => postRequest('/login', userData)
-export const logout = () => postRequest('/logout')
-export const getUserProfile = () => getRequest(`/profile`)
+export const signUp = userData => postRequest('/auth/signup', userData)
+export const login = userData => postRequest('/auth/login', userData)
+export const logout = () => postRequest('/auth/logout')
+export const getUserProfile = () => getRequest(`/auth/profile`)
 
-export const getUserInfo = username => getRequest(`/user/${username}`)
-export const getUserPosts = username => getRequest(`/user/${username}/posts`)
-export const getUserComments = username => getRequest(`/user/${username}/comments`)
-export const getUserLikes = username => getRequest(`/user/${username}/likes`)
-export const updateUserInfo = userData => putRequest('/user/update', userData)
+export const getUserInfo = username => getRequest(`/users/${username}`)
+export const getUserPosts = username => getRequest(`/users/${username}/posts`)
+export const getUserComments = username => getRequest(`/users/${username}/comments`)
+export const getUserLikes = username => getRequest(`/users/${username}/likes`)
+export const updateUserInfo = userData => putRequest('/users/update', userData)
